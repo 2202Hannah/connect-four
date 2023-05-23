e3640ebf85262a88fe4b8a4a205ca0b2286b4005
2202Hannah <hannahbarber22@hotmail.co.uk>
2023-05-23 09:18:53 +0100

    add set up for matrix generator

---
+++ connect-four/src/components/game/Game.jsx
@@ -0,0 +1,12 @@
+
+import { matrixGenerator } from "./utils"
+
+export const Game = () => {
+  return (
+    <div className="board">
+      <h1>Connect Four</h1>
+    </div>
+  );
+};
+
+export default Game;
